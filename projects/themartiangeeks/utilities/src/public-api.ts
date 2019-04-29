eb065f0d14b70ee2c73b102e06cf703090e1a3ac
Hassane Dao <dao.hassane@gmail.com>
2019-04-29 22:46:34 +0000

    Component

--- projects/themartiangeeks/utilities/src/public-api.ts
+++ projects/themartiangeeks/utilities/src/public-api.ts
@@ -8,3 +8,6 @@ export * from './lib/utilities.module';
 export * from './lib/interceptors/jwt-interceptor/jwt-interceptor';
 export * from './lib/interceptors/error-interceptor/error-interceptor';
 export * from './lib/interceptors/cache-interceptor/cache-interceptor';
+
+export * from './lib/components/page-loader/page-loader.component';
+export * from './lib/components/empty-state/empty-state.component';
